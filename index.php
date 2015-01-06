6d3fdd5c5f785ac5e2b7e55dcee3abbcdc289e7c
Ben Lieberman <blieberman@smartertravelmedia.com>
2015-01-06 02:31:10 -0500

    normalizing code spacing/tabs & removing home tab from header

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <?php include 'header.php'; ?>
-  </head>
 
-  <body>
-    <?php include_once("analyticstracking.php") ?>
-    <div class="jumbotron masthead">
-      <div class="container">
-        <h1>Triposal</h1>
-        <p>The travel search decision maker.</p>
-        <p><a data-toggle="modal" href="#planner" class="btn btn-primary btn-large">Get Started</a></p>
-          <ul class="masthead-links">
-					<center>
-            <li>Version 0.0.2</li>
-            <li>Ben Lieberman</li>
-            <li>&copy; 2015</li>
-					</center>
-          </ul>
-      </div>
+<head>
+  <?php include 'header.php'; ?>
+</head>
+
+<body>
+  <?php include_once("analyticstracking.php") ?>
+  <div class="jumbotron masthead">
+    <div class="container">
+      <h1>Triposal</h1>
+      <p>The travel search decision maker.</p>
+      <p><a data-toggle="modal" href="#planner" class="btn btn-primary btn-large">Get Started</a></p>
+        <ul class="masthead-links">
+        <center>
+          <li>Version 0.0.2</li>
+          <li>Ben Lieberman</li>
+          <li>&copy; 2015</li>
+        </center>
+        </ul>
     </div>
-    <?php include 'modals.php'; ?>
-  </body>
+  </div>
+  <?php include 'modals.php'; ?>
+</body>
+
 </html>
